8f4e389e7ec343281da60bd8a93adae2103ced00
Ben Vinegar <ben@benv.ca>
2023-08-01 19:33:25 -0400

    Initial commit: basic scene w/ player character

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * 5,
+            transform.position.y + Input.GetAxis("Vertical") * Time.deltaTime * 5,
+            transform.position.z
+        );
+    }
+}
