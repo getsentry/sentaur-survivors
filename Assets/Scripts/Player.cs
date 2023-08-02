1ebd53e22721ca29c64f6d0c693869bd65361ea6
Ben Vinegar <ben@benv.ca>
2023-08-01 20:11:20 -0400

    Rename PlayerCharacter to Player for simplicity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCharacter : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start() { }
