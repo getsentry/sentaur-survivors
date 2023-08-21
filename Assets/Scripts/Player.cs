34544fbab5552bba910454ca0def020568c1cb88
Ben Vinegar <ben@sentry.io>
2023-08-21 09:58:44 -0400

    Add basic projectiles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private Projectile _projectilePrefab;
+
     [SerializeField] private int _hitPoints = 100;
+
+    [SerializeField]
+    private float _projectileFireRate = 0.5f;
+
+    private float timeElapsedSinceLastProjectile = 0.0f;
 
     private HealthBar _healthBar;
 
     // Start is called before the first frame update
-    void Start() { 
+    void Start()
+    {
         // get a reference to the Healthbar component
         _healthBar = GameObject.Find("HealthBar").GetComponent<HealthBar>();
     }
@@ -24,6 +33,19 @@ public class Player : MonoBehaviour
             transform.position.y + Input.GetAxis("Vertical") * Time.deltaTime * 5,
             transform.position.z
         );
+
+        // fire a projectile once enough time has elapsed
+        timeElapsedSinceLastProjectile += Time.deltaTime;
+        if (timeElapsedSinceLastProjectile > _projectileFireRate)
+        {
+            timeElapsedSinceLastProjectile = 0.0f;
+
+            // instantiate a new projectile
+            var projectile = Instantiate(_projectilePrefab);
+
+            // set the projectile's position to the player's position
+            projectile.transform.position = transform.position;
+        }
     }
 
     // a collision handler that is called when the enemy collides with another object
