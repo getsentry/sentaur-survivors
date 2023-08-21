819884dd7264b826254c4d402321602d35f9b94e
Ben Vinegar <ben@sentry.io>
2023-08-21 16:56:42 -0400

    Projectiles and enemies should spawn onto Level game object

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -56,6 +56,7 @@ public class Player : MonoBehaviour
 
             // instantiate a new projectile
             var projectile = Instantiate(_projectilePrefab);
+            projectile.transform.parent = transform.parent;
 
             // projectile moves in the direction of the current mouse cursor
             var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
