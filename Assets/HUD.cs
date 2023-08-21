c16e8a3df665451411446bd8bbd21cbfd108908d
Ben Vinegar <ben@sentry.io>
2023-08-21 15:53:14 -0400

    Add time elapsed to HUD UI

--- Assets/HUD.cs
+++ Assets/HUD.cs
@@ -8,23 +8,29 @@ public class HUD : MonoBehaviour
 {
 
     private TextMeshProUGUI _scoreText;
+    private TextMeshProUGUI _timeElapsed;
 
     // Start is called before the first frame update
     void Start()
     {
         // get score text component from child
         _scoreText = transform.Find("Score").GetComponent<TextMeshProUGUI>();
+        _timeElapsed = transform.Find("TimeElapsed").GetComponent<TextMeshProUGUI>();
 
         
         EventManager.AddListener("ScoreChange", (eventData) => {
             OnScoreChange((int)eventData.Data);
-        });    
+        });
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // get time elapsed since game start in mm:ss format
+        var timeElapsed = Time.timeSinceLevelLoad;
+        var minutes = Mathf.FloorToInt(timeElapsed / 60.0f);
+        var seconds = Mathf.FloorToInt(timeElapsed % 60.0f);
+        _timeElapsed.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     private void OnScoreChange(int score)
