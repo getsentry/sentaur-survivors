7cf69fb02e1c4ef769443a8df321759da81342ba
Ben Vinegar <ben@sentry.io>
2023-08-22 10:33:28 -0400

    Make the damage text jump

--- Assets/Scripts/DamageText.cs
+++ Assets/Scripts/DamageText.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using TMPro;
+using DG.Tweening;
+
 
 public class DamageText : MonoBehaviour
 {
@@ -23,15 +25,27 @@ public class DamageText : MonoBehaviour
         _text.text = damage.ToString();
     }
 
+    private void Init(int value) {
+
+        // set the text
+        this.SetDamage(value);
+
+        // make the text jump up
+        transform.DOLocalJump(transform.localPosition, 
+            1.0f, // jump power
+            1,    // num jumps
+            0.5f  // duration
+        );
+    }
+
     // usage: _damageTextPrefab.Spawn(transform, position, 10, 2.0f);
 
-    public DamageText Spawn(Transform parent, Vector2 position, int value, float duration = 2.0f) {
+    public DamageText Spawn(Transform parent, Vector2 position, int value, float duration = 1.0f) {
         // instantiate the damage text prefab
         var damageText = Instantiate(this, parent);
         damageText.transform.localPosition = position;
 
-        // set the text
-        damageText.SetDamage(value);
+        damageText.Init(value);
 
         // destroy the damage text after a delay
         Destroy(damageText.gameObject, duration);
