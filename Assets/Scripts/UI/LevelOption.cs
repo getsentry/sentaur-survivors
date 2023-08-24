bd246c820d0485baadcd4a185f1f016d7ebd6a43
Isabella Enriquez <45607721+isabellaenriquez@users.noreply.github.com>
2023-08-23 21:16:44 -0700

    Add Level Up Sprites (#22)
    * also moved sprites out of `Upgrade` and into `LevelOption`
    
    ---------
    
    Co-authored-by: Olivier Williams <olivier.williams@sentry.io>

--- Assets/Scripts/UI/LevelOption.cs
+++ Assets/Scripts/UI/LevelOption.cs
@@ -3,20 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using TMPro;
-using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine.UI;
 
 public class LevelOption : MonoBehaviour
 {
+
+    [SerializeField]
+    [Tooltip("Sprite for the count++ upgrade")]
+    private Sprite _countSprite;
+
+    [SerializeField]
+    [Tooltip("Sprite for the speed++ upgrade")]
+    private Sprite _speedSprite;
+
+    [SerializeField]
+    [Tooltip("Sprite for the damage++ upgrade")]
+    private Sprite _damageSprite;
+
     private TextMeshProUGUI _title;
     private TextMeshProUGUI _description;
     private TextMeshProUGUI _stats;
-    // private Image _icon;
+    private Image _icon;
 
     void Awake() {
         _title = transform.Find("Title").GetComponent<TextMeshProUGUI>();
         _description = transform.Find("Description").GetComponent<TextMeshProUGUI>();
         _stats = transform.Find("Stats").GetComponent<TextMeshProUGUI>();
-        // _icon = transform.Find("Border/Icon").GetComponent<Image>();
+        _icon = transform.Find("Border/Icon").GetComponent<Image>();
     }
 
     // fyi: title -> upgrade name, description -> level, stats -> description 
@@ -24,5 +37,19 @@ public class LevelOption : MonoBehaviour
         _title.text = title;
         _description.text = description;
         _stats.text = stats;
+
+        switch(title)
+        {
+            case "count++": 
+                _icon.sprite = _countSprite;
+                break;
+            case "speed++":
+                _icon.sprite = _speedSprite;
+                break;
+            case "damage++":
+                _icon.sprite = _damageSprite;
+                break;
+            default: break;
+        }
     }
 }
