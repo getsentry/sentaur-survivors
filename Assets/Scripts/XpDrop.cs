49ace8b35f66b64856f8e32ec0e4518dc3b7d186
Ben Vinegar <ben@sentry.io>
2023-08-23 21:22:14 -0400

    XpDrops fly towards player if they come in range

--- Assets/Scripts/XpDrop.cs
+++ Assets/Scripts/XpDrop.cs
@@ -2,15 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using DG.Tweening;
+
 public class XpDrop : MonoBehaviour
 {
     private int _xp = 10;
+    private bool _moving = false;
+
+    [SerializeField]
+    private float _detectPlayerDistance = 3f;
+
+    [SerializeField]
+    private float _moveSpeed = 5f;
 
     public void SetXp(int xp)
     {
         _xp = xp;
     }
 
+    void Update() {
+        var player = Player.Instance;
+        if (player == null) {
+            return;
+        }
+        // detect if player is with 1 unit of the pickup
+        if (!_moving && Vector2.Distance(player.transform.position, transform.position) < _detectPlayerDistance)
+        {
+            _moving = true;
+        }
+
+        if (_moving) {
+            // move towards the player
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, _moveSpeed * Time.deltaTime);
+        }
+
+    }
+    
     // on trigger handler
     void OnTriggerEnter2D(Collider2D other)
     {
