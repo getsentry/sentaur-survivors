4177f6a9fdcb00cd98cfc48274d0ea413fce9641
Ben Vinegar <ben@sentry.io>
2023-08-24 19:34:50 -0400

    Add title screen w/ "New Game" and "Quit" options

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _startButton;
+
+    [SerializeField]
+    private GameObject _quitButton;
+
+    void Awake() {
+        _startButton.GetComponent<Button>().onClick.AddListener(StartGame);
+        _quitButton.GetComponent<Button>().onClick.AddListener(QuitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartGame() {
+        Debug.Log("Start Game");
+        SceneManager.LoadScene("BattleScene", LoadSceneMode.Single);
+
+    }
+
+    private void QuitGame() {
+        Debug.Log("Quit (Note this won't quit in the editor)");
+        Application.Quit();
+    }
+}
