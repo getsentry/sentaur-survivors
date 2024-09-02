e726fd1ef14c877b7eef73bb0aa48aacf9ebe197
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2024-09-01 23:47:26 -0400

    Add ProjectileCount upgrade path

---
+++ Assets/Scripts/Weapons/GlobalUgrades/DamageUpgradePath.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class DamageUpgradePath : UpgradePathBase
+{
+    [SerializeField]
+    private float[] _damageModifiersPerLevel = { 1.3f, 1.25f, 1.25f };
+
+    public override void UpgradeToLevel(int level)
+    {
+        var weaponManager = Player.Instance.WeaponManager;
+        switch (_level)
+        {
+            case 1:
+                weaponManager.GlobalDamageModifier *= _damageModifiersPerLevel[0];
+                break;
+            case 2:
+                weaponManager.GlobalDamageModifier *= _damageModifiersPerLevel[1];
+                break;
+            case 3:
+                weaponManager.GlobalDamageModifier *= _damageModifiersPerLevel[2];
+                break;
+        }
+    }
+}
+/*
+
+            {
+                UpgradeType.DamageUp,
+                new UpgradePath(new List<string> { "+30% damage", "+60% damage", "+100% damage!" })
+            },*/
