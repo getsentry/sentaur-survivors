cad412ba1e2fa9a38a649d598c2dee4bea9e1a24
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2024-09-02 00:07:42 -0400

    Add Cooldown upgradepath

---
+++ Assets/Scripts/Weapons/GlobalUgrades/CooldownUpgradePath.cs
@@ -0,0 +1,21 @@
+class CooldownUpgradePath : UpgradePathBase
+{
+    private float[] _cooldownModifiersPerLevel = { 0.8f, 0.75f, 0.5f };
+
+    public override void UpgradeToLevel(int level)
+    {
+        var weaponManager = FindObjectOfType<WeaponManager>();
+        switch (level)
+        {
+            case 1:
+                weaponManager.GlobalCooldownModifier *= _cooldownModifiersPerLevel[0];
+                break;
+            case 2:
+                weaponManager.GlobalCooldownModifier *= _cooldownModifiersPerLevel[1];
+                break;
+            case 3:
+                weaponManager.GlobalCooldownModifier *= _cooldownModifiersPerLevel[2];
+                break;
+        }
+    }
+}
