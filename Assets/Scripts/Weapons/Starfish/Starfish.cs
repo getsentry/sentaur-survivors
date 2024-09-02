d25e18ea37c05c4daacccfb013f4cb0d45286469
Ben Vinegar <2153+benvinegar@users.noreply.github.com>
2024-09-01 23:28:28 -0400

    Add Starfish WeaponUpgradePath

--- Assets/Scripts/Weapons/Starfish/Starfish.cs
+++ Assets/Scripts/Weapons/Starfish/Starfish.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Starfish : WeaponBase
 {
     [SerializeField]
-    private float _duration = 5f;
+    public float Duration = 5f;
 
     [SerializeField]
     private StarfishProjectile _starfishProjectilePrefab;
@@ -11,13 +11,13 @@ public class Starfish : WeaponBase
     public void Start()
     {
         // hack; when starfish inits, time elapsed starts at duration (see CanFire note)
-        _timeElapsedSinceLastFire = _duration;
+        _timeElapsedSinceLastFire = Duration;
     }
 
     public override bool CanFire()
     {
         // starfish cooldown doesn't reset until duration is over
-        return _isEnabled && _timeElapsedSinceLastFire >= Cooldown + _duration;
+        return _isEnabled && _timeElapsedSinceLastFire >= Cooldown + Duration;
     }
 
     public override void Fire()
@@ -30,26 +30,8 @@ public class Starfish : WeaponBase
         for (int i = 0; i < Count; i++)
         {
             var starfish = Instantiate(_starfishProjectilePrefab);
-            starfish.Initialize(Damage, _duration, degreesBetweenStarfish * i);
+            starfish.Initialize(Damage, Duration, degreesBetweenStarfish * i);
             starfish.transform.parent = this.transform;
-        }
-    }
-
-    public void Upgrade(int level)
-    {
-        if (level == 1)
-        {
-            _isEnabled = true;
-        }
-        else if (level == 2)
-        {
-            _duration *= 1.2f;
-            BaseDamage *= 1.2f;
-        }
-        else if (level == 3)
-        {
-            _duration *= 1.5f;
-            BaseCooldown *= 0.7f;
         }
     }
 }
